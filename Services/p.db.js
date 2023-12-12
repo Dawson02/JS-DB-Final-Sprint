6b8f7b5654300d1e5358643b6b64f6984249733f
Dawson <127239947+Dawson02@users.noreply.github.com>
2023-12-12 11:38:32 -0330

    Add files via upload

---
+++ Services/p.db.js
@@ -0,0 +1,11 @@
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'Final Sprint - JavaScript/Databases',
+    password: 'Skiff_21',
+    port: 5432,
+});
+
+module.exports = pool;
